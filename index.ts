0245e35c78312430628d3bcea69244ce3825f796
Konrad Konkel <konrad.konkel@wp.pl>
2023-12-18 11:07:05 +0000

    9.9

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+// To not have eslint screaming unsafe call of any and to not use ignore this line
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 
 app.use(express.json());
 
@@ -8,7 +12,7 @@ app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
