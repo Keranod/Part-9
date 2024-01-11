16bb27a7c63e401e8c0c5e00470acb67c96ae8c1
Konrad Konkel <konrad.konkel@wp.pl>
2024-01-11 15:21:22 +0000

    9.25

--- src/components/PatientDetailsPage/index.tsx
+++ src/components/PatientDetailsPage/index.tsx
@@ -4,8 +4,12 @@ import { useParams } from 'react-router-dom';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import WorkIcon from '@mui/icons-material/Work';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-import { Patient, Diagnosis } from "../../types";
+import { Patient, Diagnosis, Entry } from "../../types";
 
 import patientService from "../../services/patients";
 import diagnosesService from '../../services/diagnoses';
@@ -39,7 +43,7 @@ const PatientDetailsPage = () => {
                 console.log(error.message);
             }
         }
-      },);
+      }, [id]);
 
     if (patientWithId === undefined) {
         return (
@@ -52,6 +56,73 @@ const PatientDetailsPage = () => {
             <p>Loading diagnoses data...</p>
         );
     }
+
+    interface entryDiagnosesProps {
+        entry : Entry
+        diagnoses: Diagnosis[]
+    }
+
+    const EntryDiagnoses = ({ entry, diagnoses }: entryDiagnosesProps) => {
+        return (
+            <div>
+                {entry.diagnosisCodes?.length === 0 
+                            ? <p>No diagnoses</p>
+                            : (
+                                <ul>
+                                    {entry.diagnosisCodes?.map(diagnose => (
+                                        <li key={diagnose}>
+                                            {diagnose}
+                                            {' '}
+                                            {diagnoses.find(d => d.code === diagnose)?.name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )
+                }
+            </div>
+        );
+    };
+
+    interface EntryDetailsProps {
+        entry: Entry;
+    }
+
+    const EntryDetails = ({ entry }: EntryDetailsProps) => {
+        return (
+            <div style={{ border: '1px solid #000', padding: '10px', margin: '10px' }}>
+                <div>
+                    {entry.date} 
+                    {
+                        entry.type === 'OccupationalHealthcare'
+                        ? (<><WorkIcon/> {entry.employerName}</>)
+                        : entry.type === 'HealthCheck'
+                        ? (<>
+                            <MedicalInformationIcon/>
+                            <p>
+                                {entry.healthCheckRating === 0
+                                ? <FavoriteIcon style={{ color: 'green' }}/>
+                                : entry.healthCheckRating === 1
+                                ? <FavoriteIcon style={{ color: 'yellow' }}/>
+                                : entry.healthCheckRating === 2
+                                ? <FavoriteIcon style={{ color: 'pink' }}/>
+                                : entry.healthCheckRating === 3
+                                ? <FavoriteIcon style={{ color: 'red' }}/>
+                                : <>Error</>
+                                }
+                            </p>
+                            </>
+                        )
+                        : entry.type === 'Hospital'
+                        ? <LocalHospitalIcon/>
+                        : <>Error</>
+                    }
+                </div>
+                <i>{entry.description}</i>
+                <EntryDiagnoses entry={entry} diagnoses={diagnoses}/>
+                <p>diagnose by {entry.specialist}</p>
+            </div>
+        );
+    };
 
     return (
         <div>
@@ -72,24 +143,7 @@ const PatientDetailsPage = () => {
                 patientWithId.entries.length === 0
                 ? <p>No entires</p>
                 : patientWithId.entries.map(entry => (
-                    <div key={entry.id}>
-                        <p>{entry.date} <i>{entry.description}</i></p>
-                        {
-                            entry.diagnosisCodes?.length === 0 
-                            ? <p>No diagnoses</p>
-                            : (
-                                <ul>
-                                    {entry.diagnosisCodes?.map(diagnose => (
-                                        <li key={diagnose}>
-                                            {diagnose}
-                                            {' '}
-                                            {diagnoses.find(d => d.code === diagnose)?.name}
-                                        </li>
-                                    ))}
-                                </ul>
-                            )
-                        }
-                    </div>
+                    <EntryDetails key={entry.id} entry={entry}/>
                 ))
             }
         </div>
